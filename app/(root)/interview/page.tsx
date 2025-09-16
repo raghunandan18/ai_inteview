4a002c21a0771e857de0dbb40309ee0cf3c87259
raghunandan18 <raghunandan56565@gmail.com>
2025-09-16 16:21:24 +0530

    Refactor interview page and layout; add Agent component
    - Deleted unused interview layout file
    - Updated interview page logic and structure
    - Added new Agent UI component

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,8 +1,16 @@
+import Agent from '@/components/ui/Agent';
 import React from 'react';
 
 const InterviewPage = () => {
     return (
-        <div className="interview-page"></div>
+        <>
+            <h3 className="interview-page"></h3>
+
+            <Agent userName = 'you' userId = " user1" type = "generate"/>
+
+        </>
+        
+
     )
 }   
 export default InterviewPage;
